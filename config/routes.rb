81bd74ae4b202abf5711113980e679ba3120d4a5
taro <taro0111japan@yahoo.co.jp>
2017-10-19 16:20:06 +0900

    Create create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
   get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
+  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
 end
